5ad00559abaac572e3d163abe681a5dabbbb7e1d
Mikkel Hoegh <mikkel@hoegh.org>
2012-03-06 21:33:26 +0100

    Remove another swath of unused code.

--- drupal/js/opeka.common.js
+++ drupal/js/opeka.common.js
@@ -325,121 +325,6 @@ var Opeka = { status: {}},
     }
   };
 
-
-/*
-  // Method used in order to print the final message when the chat room
-  // has been closed.
-  now.displayError = function (error) {
-    $('#errors').html(error);
-    $('#errors').dialog();
-  };
-
-  // Method used in order to effect a local removal of all the messages
-  // of a single user.
-  now.localDeleteAllMsg = function (clientId) {
-    $('#chat-message-list').find("."+clientId).html('');
-  };
-
-  //This method is used in order to leave a room
-  now.quitRoom = function(callback){
-    opeka.activeRoomId = null;
-    opeka.closeChat();
-    window.location.replace("#");
-
-    if (callback) {
-      callback();
-    }
-  };
-
-  // This method is used in order to join a room
-  now.joinRoom = function(roomId, callback){
-    opeka.closeChat();
-    opeka.activeRoomId = roomId;
-    opeka.openChat(roomId);
-    window.location.replace("#room="+roomId);
-    if (callback) {
-      callback();
-    }
-  };
-
-
-  // This method is used in order to update the active room of the users
-  // in case a counselor have deleted it.
-  now.updateActiveRoom = function(){
-  if (opeka.activeRoomId && !opeka.rooms[opeka.activeRoomId]){
-        now.changeRoom(null);
-        opeka.activeRoomId = null;
-        opeka.closeChat();
-  }
-  };
-
-  // Open the chat interface, if not open already.
-  opeka.openChat = function (roomId) {
-    // If the chat is open already or room was not found, do nothing.
-    if (opeka.chatIsOpen || !opeka.rooms || !opeka.rooms[roomId]) { return; }
-
-    var room = opeka.rooms[roomId];
-
-    $('#chat-message-list')
-      // Remove the existing chat messages.
-      .children().remove().end();
-
-    $('#opeka-chat').fadeIn();
-
-    opeka.chatIsOpen = true;
-
-    $('#opeka-chat').find('#th-room').html('Opeka Chat - Room: ' + room.name);
-  };
-
-  // Open the chat interface, if not open already.
-  opeka.closeChat = function () {
-    $('#opeka-chat').fadeOut();
-    opeka.chatIsOpen = false;
-  };
-
-  // When the history state changes, check if we need to open/close a
-  // room or similar changes.
-  $(window).bind('hashchange', function(event) {
-    var roomId = $.bbq.getState('room');
-
-    // We have change to a chat room.
-    if (roomId) {
-      now.changeRoom(roomId, function(addingStatus){
-        if (addingStatus === 'OK') {
-          opeka.closeChat();
-          opeka.activeRoomId = roomId;
-          opeka.openChat(roomId);
-        } else if(addingStatus < 0){
-          now.displayError('You cannot join this room.');
-          opeka.closeChat();
-          window.location.replace("#");
-        } else{
-        now.displayError('You have been added to queue with position: '+addingStatus);
-            opeka.closeChat();
-        }
-      });
-    }
-    else {
-      if (opeka.activeRoomId) {
-        now.changeRoom(null);
-        opeka.activeRoomId = null;
-        opeka.closeChat();
-      }
-    }
-  });
-
-  // Handle the send chat message for both admins and guests.
-  $("#opeka-send-message").live('click', function (event) {
-    var message = $('#opeka-chat-message').val().trim();
-
-    if (opeka.activeRoomId && message) {
-      now.sendMessageToRoom(opeka.activeRoomId, message);
-    $('#opeka-chat-message').val('');
-    }
-
-    event.preventDefault();
-  });
-*/
   // Sign in to the chat app.
   Opeka.signIn = function (user, callback) {
     now.signIn(user, function () {
@@ -456,7 +341,6 @@ var Opeka = { status: {}},
       }
     });
   };
-
 
   // Basic setup for the app when the DOM is loaded.
   $(function () {
