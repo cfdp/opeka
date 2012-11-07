8fcfd58d404dd78e2a94f1b341137ae4dd40e485
kreativt team <kreativt-team@cyberhus.dk>
2012-11-07 13:03:31 +0100

    update on showing rooms for admin when hidePairRooms is true

--- drupal/js/opeka.views.js
+++ drupal/js/opeka.views.js
@@ -1054,6 +1054,7 @@
         html += JST.opeka_pair_room_list_tmpl({
           admin: _.isFunction(now.isAdmin),
           labels: {
+          placeholder: Drupal.t('No rooms created'),
           enterRoom: Drupal.t('Enter')
         },
           rooms: roomList
