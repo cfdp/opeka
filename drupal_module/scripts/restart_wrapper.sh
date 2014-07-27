3e8888a8455ea6f4ed0623b675f82b35942546a7
Benjamin Christensen <benjamin@benway.dk>
2014-07-27 20:59:14 +0000

    Adding restart functionality

--- drupal_module/scripts/restart_wrapper.sh
+++ drupal_module/scripts/restart_wrapper.sh
@@ -2,7 +2,27 @@
 # Wrapper script called by Drupal to restart the chat app, in this case via Monit.
 # For this to work, you need to edit your sudoers file to allow www-data to run the
 # Monit restart command.
-echo -e "Restarting the chat, this will take a few seconds... \n"
-sudo /usr/bin/monit cfdpchat-app restart
+
+# Log to console and backup.log
+# cd /home/ubuntu/programs/cfdpchat/drupal_module/scripts
+# exec >  >(tee -a /home/ubuntu/programs/cfdpchat/drupal_module/scripts/restart.log)
+# exec 2> >(tee -a /home/ubuntu/programs/cfdpchat/drupal_module/scripts/restart.log >&2)
+
+echo -e "Restarting the chat, this will take a few seconds... arg: $1 \n"
+if [ "$1" == "http://ch.curachat.com" ]
+then
+  echo "chat is ch"
+  sudo /usr/bin/monit restart cyberhuschat
+elif [ "$1" == "http://kram.curachat.com" ]
+then
+  echo "chat is kram"
+  sudo /usr/bin/monit restart kramchat
+elif [ "$1" == "http://netstof.curachat.com" ]
+then
+  echo "chat is netstof"
+  sudo /usr/bin/monit restart netstofchat
+else
+  echo "Chat not identified - please contact support."
+fi
 
 
