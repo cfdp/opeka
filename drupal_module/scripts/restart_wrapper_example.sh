88f132a15f109f7145eb8db29c5acb086e587045
Benjamin Christensen <benjamin@benway.dk>
2014-09-05 10:23:14 +0000

    Adding restart script
    Makes it possible for coordinators to restart the chat via the admin interface

--- drupal_module/scripts/restart_wrapper_example.sh
+++ drupal_module/scripts/restart_wrapper_example.sh
@@ -4,20 +4,18 @@
 # Monit restart command.
 
 # Log to console and backup.log
-# cd /home/ubuntu/programs/cfdpchat/drupal_module/scripts
-# exec >  >(tee -a /home/ubuntu/programs/cfdpchat/drupal_module/scripts/restart.log)
-# exec 2> >(tee -a /home/ubuntu/programs/cfdpchat/drupal_module/scripts/restart.log >&2)
+# cd /home/path/to/your/log
+# exec >  >(tee -a /home/path/to/your/log/restart.log)
+# exec 2> >(tee -a /home/path/to/your/log/restart.log >&2)
 
 echo -e "Restarting the chat, this will take a few seconds... \n"
-if [ "$1" == "http://ch.curachat.com" ]
+if [ "$1" == "http://yourdomain" ]
 then
-  sudo /usr/bin/monit restart cyberhuschat
+  sudo /usr/bin/monit restart yourchat
 elif [ "$1" == "http://kram.curachat.com" ]
 then
-  sudo /usr/bin/monit restart kramchat
+  sudo /usr/bin/monit restart yourotherchat
 elif [ "$1" == "http://netstof.curachat.com" ]
-then
-  sudo /usr/bin/monit restart netstofchat
 else
   echo "Chat not identified - please contact support."
 fi
