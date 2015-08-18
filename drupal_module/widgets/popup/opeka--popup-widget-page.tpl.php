d04f17de0360eb301c22482051c7b62c7a254ddd
Benjamin Christensen <benjamin@greenvolcano.dk>
2015-08-18 15:11:56 +0200

    Incorporate widget functionality in Drupal module
    Improves UX for updating widget content - can now be done via Drupal admin pages

---
+++ drupal_module/widgets/popup/opeka--popup-widget-page.tpl.php
@@ -0,0 +1,12 @@
+<div class="status-wrapper popup">
+  <div class="status-tab"></div>
+  <div class="status-content">
+    <p>
+      <?php print render($variables['opeka_schedule']); ?>
+    </p>
+  </div>
+  <div class="login-button">
+    <a id="join-pair-chat" class="btn pairchat chat" href="#"></a>
+  </div>
+</div>
+
