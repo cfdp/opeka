41eb80c346ddc7fd80b69b305704e47ffcebde13
Benjamin Christensen <benjamin@greenvolcano.dk>
2015-10-10 15:43:45 +0200

    Adding group widget functionality and refactoring styling with SASS II

---
+++ drupal_module/widgets/foldout/js/foldoutController.js
@@ -0,0 +1,74 @@
+/**
+ * Script for controlling the foldout chat window, to be embedded on client site
+ * @todo: implement as Drupal behaviors
+ */
+(function($) {
+  var foldoutLocation = "body"; // Enter the DOM location for foldout
+  var opekaChatURL = opekaFoldoutBaseURL + "/opeka-widgets/foldout?client_url=" + opekaFoldoutClientURL;
+  
+  /* Check if there's any css files to add. The cssFiles global is initialized in embed.html */
+  if (typeof opekaCssFiles !== 'undefined' && opekaCssFiles.length > 0) {
+    // the array is defined and has at least one element
+      $.each( opekaCssFiles, function( i, val ) {
+        addOpekaFoldoutCss(opekaCssFiles[i][0], opekaCssFiles[i][1]);
+      });
+  }
+
+  /* Add custom CSS file to HEAD */
+  function addOpekaFoldoutCss(cssId, cssPath) {
+    if (!document.getElementById(cssId)) {
+        var head  = document.getElementsByTagName('head')[0];
+        var link  = document.createElement('link');
+        link.id   = cssId;
+        link.rel  = 'stylesheet';
+        link.type = 'text/css';
+        link.href = cssPath+cssId;
+        link.media = 'all';
+        head.appendChild(link);
+    }
+  };
+
+  // Insert Iframe element
+  $( foldoutLocation ).append( '<div class="opeka-chat-foldout-wrapper"><div id="opeka-chat-iframe"><iframe src="' + opekaChatURL + '" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" height="200"></iframe></div></div>' );
+
+  /**
+   * Foldout animation
+   */
+  $(".opeka-chat-foldout-wrapper").hover(
+    function() {
+      $(this).stop(true,true).animate({
+        right: 0
+      },200);
+    },
+    function() {
+      $(this).stop(true,true).animate({
+        right: -260
+      },200);
+    }
+  );
+
+/*  function foldoutController(foldoutAction) {
+    if ( popupAction === "Activate") {
+      // @todo: some effect
+    else if (popupAction === "Deactivate") {
+      // @todo: some effect
+    }
+  }*/
+
+  /**
+   * Receive activate or deactive messages from the iframe
+   * https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
+   */
+/*  window.addEventListener("message", receiveMessage, false);
+
+  function receiveMessage(event)
+  {
+    if (event.origin !== opekaFoldoutBaseURL) {
+      console.log("Bad window");
+      return;
+    }
+    else {
+      foldoutController(event.data);
+    }
+  }*/
+})(jQuery);
---
+++ drupal_module/widgets/foldout/opeka--foldout-widget-page.tpl.php
@@ -0,0 +1,13 @@
+<div class="name-tab"><?php print render($variables['opeka_pair_chat_name']); ?></div>
+<div class="status-wrapper foldout">
+  <div class="status-tab"></div>
+  <div class="status-content">
+    <p>
+      <?php print render($variables['opeka_schedule']); print "jeja" . render($vars['opeka_widget_roomtype']); ?>
+    </p>
+  </div>
+  <div class="login-button">
+    <a id="join-pair-chat" class="btn pairchat chat" href="#"></a>
+  </div>
+</div>
+
