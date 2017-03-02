dfaff851a20f19f9c9242d6fff79b1a4f00b8ddd
Benjamin Christensen <udvikling@greenvolcano.dk>
2017-03-02 12:52:37 +0100

    Fix for issue with room not being initialized

--- nodejs/lib/opeka.js
+++ nodejs/lib/opeka.js
@@ -494,21 +494,16 @@ function Server(config, logger) {
   self.everyone.addServerMethod('writingMessage', function (roomId, callback) {
     var client = this,
     room = opeka.rooms.list[client.activeRoomId];
-    if (room) {
-      if (_.has(room, 'users')) {
-        var userInRoom = room.users[client.clientId];
-        if (!_.isEmpty(userInRoom)) {
-          userInRoom.writes = roomId.status;
-        }
-        var writers = _.where(room.users, {'writes': true});
-        writers = _.map(writers, function (keys, value) {
-          return keys.name;
-        });
-        self.sendWritesMessage(writers, room.group);
+    if (room && _.has(room, 'users')) {
+      var userInRoom = room.users[client.clientId];
+      if (!_.isEmpty(userInRoom)) {
+        userInRoom.writes = roomId.status;
       }
-    }
-    else {
-      self.logger.info('@debug: room not defined (writingMessage)');
+      var writers = _.where(room.users, {'writes': true});
+      writers = _.map(writers, function (keys, value) {
+        return keys.name;
+      });
+      self.sendWritesMessage(writers, room.group);
     }
   });
 
