b488a344cdbb4a81fb7df6653fd9c690d850bd36
Benjamin Christensen <udvikling@greenvolcano.dk>
2017-03-07 12:42:20 +0000

    Better responsive behavior of the widgets

--- drupal_module/widgets/inline/opeka--inline-widget-page.tpl.php
+++ drupal_module/widgets/inline/opeka--inline-widget-page.tpl.php
@@ -1,8 +1,6 @@
 <div class="wrapper">
   <div class="status-content">
-    <p>
-      <?php print render($variables['opeka_schedule']); ?>
-    </p>
+    <?php print render($variables['opeka_schedule']); ?>
   </div>
   <div class="login-button">
     <a id="join-chat" class="btn chat" href="#"></a>
