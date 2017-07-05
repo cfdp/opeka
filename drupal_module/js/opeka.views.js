e9b4d0fcb99455b30950ee5c21eede0e36e1ac5b
Benjamin Christensen <udvikling@greenvolcano.dk>
2017-07-05 22:22:32 +0200

    Add the option of making screening questions required
    Forces the user to actively choose an option before entering the chat

--- drupal_module/js/opeka.views.js
+++ drupal_module/js/opeka.views.js
@@ -1958,9 +1958,18 @@
       //add a random number to each anonymous user to help in distinguishing them
       var x = Math.floor((Math.random() * 50) + 1);
 
-      var question = this.$el.find('.screening-question').text();
+      var question = this.$el.find('p.screening-question').text();
       var answer = this.$el.find('input[name=screening]:checked').val();
-      // @todo: save other answer options as well
+      var screeningRequired = (Drupal.settings.opeka_screening && Drupal.settings.opeka_screening.opeka_screening_required);
+      var validationError;
+
+      var errMsg = Drupal.t('You must answer the question before you can enter the chat.');
+      // Check if an option has been chosen if answering is required
+      if (screeningRequired && (answer === undefined) && !user.admin) {
+        alert(errMsg);
+        validationError = true;
+      }
+
       user.nickname = this.$el.find('.nickname').val() || Drupal.t('Anonymous!x', {'!x': x});
       user.age = this.$el.find('.age').val();
       user.gender = this.$el.find('.gender').val();
@@ -1969,12 +1978,14 @@
       user.roomId = user.roomId ? user.roomId : this.roomId;
       user.queueId = this.queueId;
 
-      Opeka.signIn(user, function () {
-        view.$el.fadeOut();
-        $(window).bind('beforeunload.opeka', function () {
-          return Drupal.t('Do you really want to leave this page?');
+      if (!validationError) {
+        Opeka.signIn(user, function () {
+          view.$el.fadeOut();
+          $(window).bind('beforeunload.opeka', function () {
+            return Drupal.t('Do you really want to leave this page?');
+          });
         });
-      });
+      }
 
       if (event) {
         event.preventDefault();
